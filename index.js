c57088d5e97c6405a3f0278faf1afe9c8f62620e
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-08-18 10:56:07 +0900

    prisma db upload

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const express = require("express");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
 const client = new PrismaClient();
-
 const selenium = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
-const chromedriver = require("chromedriver");
-console.log(chromedriver.path);
-chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
 const PORT = 8080;
 const HOST = `192.168.0.4`;
@@ -23,61 +19,68 @@ const driver = new selenium.Builder()
   .setChromeOptions(new chrome.Options().headless())
   .build();
 
-async function mySelenium() {
-  try {
-    await driver.get("https://polyhaven.com/models");
-    console.log("드라이버 실행");
-    driver
-      .executeScript(
-        `window.scrollTo({top: document.body.scrollHeight, behavior:"smooth"})`
-      )
-      .then(
-        setTimeout(async () => {
-          await driver
-            .findElements(
-              selenium.By.xpath(`//a[@class="GridItem_gridItem__0cuEz"]`)
-            )
-            .then(async function (productAnchor) {
-              for (var i = 1; i < productAnchor.length; i++) {
-                productAnchor[i].getAttribute(`href`).then(function (href) {
-                  href = href;
-                });
-                console.log(i);
+app.get("/db", (req, res) => {
+  res.send("DB페이지11\n");
+  async function mySelenium() {
+    try {
+      await driver.get("https://polyhaven.com/models");
+      console.log("드라이버 실행");
+      driver
+        .executeScript(
+          `window.scrollTo({top: document.body.scrollHeight, behavior:"smooth"})`
+        )
+        .then(
+          setTimeout(async () => {
+            await driver
+              .findElements(
+                selenium.By.xpath(`//a[@class="GridItem_gridItem__0cuEz"]`)
+              )
+              .then(async function (productAnchor) {
+                for (var i = 1; i < productAnchor.length; i++) {
+                  var hrefLink;
+                  productAnchor[i].getAttribute(`href`).then(function (href) {
+                    hrefLink = href;
+                  });
+                  console.log(i);
 
-                const div = await productAnchor[i].findElement(
-                  selenium.By.className("GridItem_thumb__M8icc")
-                );
+                  const div = await productAnchor[i].findElement(
+                    selenium.By.className("GridItem_thumb__M8icc")
+                  );
 
-                const img = await div.findElement(selenium.By.css("img"));
+                  const img = await div.findElement(selenium.By.css("img"));
 
-                const src = await img.getAttribute(`src`);
+                  const src = await img.getAttribute(`src`);
 
-                const alt = await img.getAttribute(`alt`);
+                  const alt = await img.getAttribute(`alt`);
+                  const asset = await client.assets.upsert({
+                    where: {
+                      assetName: alt,
+                    },
+                    create: {
+                      id: i,
+                      assetName: alt,
+                      photoUrl: src,
+                      linkUrl: hrefLink,
+                    },
+                    update: {},
+                  });
+                  console.log(asset);
 
-                const asset = await client.assets.upsert({
-                  create: {
-                    id: i,
-                    assetName: alt,
-                    photoUrl: src,
-                    linkUrl: src,
-                  },
-                });
-                console.log(asset);
-              }
-            });
-        }, 1000)
-      );
-  } catch (error) {
-    console.log(error);
-  } finally {
-    setTimeout(() => {
-      driver.quit();
-    }, 5000);
+                  /*                console.log(`assetName : ${alt}`);
+                  console.log(`photoUrl : ${src}`);
+                  console.log(`linkUrl : ${hrefLink}`); */
+                }
+              });
+          }, 1000)
+        );
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setTimeout(() => {
+        driver.quit();
+      }, 200000);
+    }
   }
-}
-
-app.get("/", (req, res) => {
-  res.send("DB페이지\n");
   mySelenium();
   console.log("셀레니움 끝");
 });
